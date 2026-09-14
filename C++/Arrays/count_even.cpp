17bbddd5a903d3870f9598ab1c68e68962814b26
Nihan Shetty <nihanshetty.tech5@gmail.com>
2026-09-14 16:00:20 +0530

    Add array practice

--- C++/Arrays/count_even.cpp
+++ C++/Arrays/count_even.cpp
@@ -11,15 +11,15 @@ int main(){
         cin>>arr[i];
     }
 
-    int count = 0;
+    int count_even = 0;
 
     for(int i=0;i<n;i++){
         if(arr[i]%2 == 0){
-            count++;
+            count_even++;
         }
     }
 
-    cout<<count<<endl;
+    cout<<count_even<<endl;
 
     return 0;
 }
